ff9f951923ae64db4c4d2bca6c35ce361f163f86
Michael Bleigh <bleigh@google.com>
2024-08-22 14:57:21 -0700

    fix ts problems

--- src/home/client/main.tsx
+++ src/home/client/main.tsx
@@ -3,9 +3,9 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import AnalyzeImage from "../../analyzeImage/client";
-import MadLibs from "../../madlibs/client";
+import Home from "./Home.js";
+import AnalyzeImage from "../../analyzeImage/client/index.js";
+import MadLibs from "../../madlibs/client/index.js";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
