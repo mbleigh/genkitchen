f35903ee9389cafc717fb58f2242ba233bc64068
Michael Bleigh <bleigh@google.com>
2024-08-22 15:06:08 -0700

    tsc

--- src/common/server/genkit.ts
+++ src/common/server/genkit.ts
@@ -1,12 +1,12 @@
 import { configureGenkit } from "@genkit-ai/core";
 import { dotprompt } from "@genkit-ai/dotprompt";
 import { googleAI } from "@genkit-ai/googleai";
-import { vertexAI } from "@genkit-ai/vertexai";
+// import { vertexAI } from "@genkit-ai/vertexai";
 
 configureGenkit({
   plugins: [
     googleAI(),
-    vertexAI({ location: "us-central1", projectId: "bleigh-genkit-test" }),
+    // vertexAI({ location: "us-central1", projectId: "bleigh-genkit-test" }),
     dotprompt(),
   ],
   logLevel: "warn",
