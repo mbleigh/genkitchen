d7c4a77a48dd86addb6ab5ee1c010c005ba3411b
Michael Bleigh <bleigh@google.com>
2024-08-22 14:35:22 -0700

    fix stuff

--- src/common/server/util.ts
+++ src/common/server/util.ts
@@ -1,8 +1,14 @@
+import { z } from "@genkit-ai/core/schema";
 import { Flow, runFlow } from "@genkit-ai/flow";
 import { IRouter, Request, Response } from "express";
 
-export function flowRoute<I,O,S>(app: IRouter, path: string, flow: Flow<I,O,S>) {
-  return async (req: Request, res: Response) {
-    res.json(await runFlow<I,O,S>(flow, req.body));
-  }
+export function flowRoute<I extends z.ZodTypeAny, O extends z.ZodTypeAny, S extends z.ZodTypeAny>(
+  app: IRouter,
+  path: string,
+  flow: Flow<I, O, S>
+) {
+  app.post(path, async (req: Request, res: Response) => {
+    res.json(await runFlow<I, O, S>(flow, req.body));
+  });
+  return app;
 }
